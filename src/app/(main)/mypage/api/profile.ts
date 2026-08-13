017b77e18812fc6b7370e0d5a64d19b845999147
ryosuke_naijo <naijo3250@gmail.com>
2026-08-13 13:30:26 +0900

    共有されていたのをプロフィールを、ユーザーごとに保存する処理に変更

--- src/app/(main)/mypage/api/profile.ts
+++ src/app/(main)/mypage/api/profile.ts
@@ -1,7 +1,6 @@
 import type { ProfileFormData } from "../types/user";
 
 // localStorageで使うキー
-const PROFILE_STORAGE_KEY = "userProfile";
 const CURRENT_USER_STORAGE_KEY = "currentUser";
 
 type CurrentUser = {
@@ -10,43 +9,66 @@ type CurrentUser = {
   email: string;
 }
 
+const getCurrentUser = (): CurrentUser | null => {
+  const savedCurrentUser = localStorage.getItem(
+    CURRENT_USER_STORAGE_KEY,
+  );
+
+  if(!savedCurrentUser){
+    return null;
+  }
+
+  return JSON.parse(savedCurrentUser) as CurrentUser;
+};
+
+const getProfileStorageKey = (
+  userId: string,
+): string => {
+  return `userProfile-${userId}`;
+};
+
 // ユーザー情報を取得する
 export const getProfile = (): ProfileFormData | null => {
+  const currentUser = getCurrentUser();
+
+  if(!currentUser){
+    return null;
+  }
+  
+  const profileStrageKey = getProfileStorageKey(currentUser.id);
+
   const savedProfile = localStorage.getItem(
-    PROFILE_STORAGE_KEY,
+    profileStrageKey,
   );
 
   if(savedProfile) {
     return JSON.parse(savedProfile) as ProfileFormData;
   };
 
-  const savedCurrentUser = localStorage.getItem(
-    CURRENT_USER_STORAGE_KEY,
-  )
-
-  // 保存されたデータがない場合
-  if (!savedCurrentUser) {
-    return null;
-  };
-
-  const currentUser = JSON.parse(
-    savedCurrentUser,
-  ) as CurrentUser;
 
   // 保存された文字列をオブジェクトに戻して返す
   return {
     name: currentUser.name,
     email: currentUser.email,
     introduction: "",
-  }
+  };
 };
 
 // ユーザー情報を更新する
 export const updateProfile = (
   data: ProfileFormData,
 ): void => {
-  localStorage.setItem(
-    PROFILE_STORAGE_KEY,
-    JSON.stringify(data),
-  );
+  const currentUser = getCurrentUser();
+
+  if(!currentUser){
+    return;
+  }
+
+  const profileStrageKey = getProfileStorageKey(currentUser.id);
+
+  // ユーザー情報を更新する
+    localStorage.setItem(
+      profileStrageKey,
+      JSON.stringify(data),
+    );
 };
